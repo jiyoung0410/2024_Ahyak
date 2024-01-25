84a09b56b011ee1ab0686b844888889c543878f4
jiyoung <ghkd1310@naver.com>
2024-01-26 00:44:27 +0900

    0126 today 페이지 주간 달력 리사이클러뷰

---
+++ app/src/main/java/com/example/ahyak/CalenderAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.ahyak
+
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ahyak.databinding.ItemTodayWeekCalenderBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+class CalendarAdapter(private val cList: List<CalendarVO>) :
+    RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder>() {
+
+    class CalendarViewHolder(private val binding: ItemTodayWeekCalenderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: CalendarVO) {
+            binding.date.text = item.cl_date
+            binding.day.text = item.cl_day
+
+            var today = binding.date.text
+
+            // 오늘 날짜
+            val now = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                LocalDate.now().format(DateTimeFormatter.ofPattern("dd").withLocale(Locale.forLanguageTag("ko")))
+            } else {
+                TODO("VERSION.SDK_INT < O")
+            }
+            // 오늘 날짜와 캘린더의 오늘 날짜가 같을 경우 background_blue 적용하기
+            if (today == now) {
+                binding.weekCardview.setBackgroundResource(R.drawable.today_week_calender_style_select)
+                binding.date.setTextColor(ContextCompat.getColor(binding.root.context, R.color.point))
+                binding.day.setTextColor(ContextCompat.getColor(binding.root.context, R.color.point))
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
+        val binding = ItemTodayWeekCalenderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CalendarViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
+        holder.bind(cList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return cList.size
+    }
+
+}
--- app/src/main/java/com/example/ahyak/TodayRecordFragment.kt
+++ app/src/main/java/com/example/ahyak/TodayRecordFragment.kt
@@ -1,59 +1,83 @@
 package com.example.ahyak
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.ahyak.databinding.FragmentTodayRecordBinding
+import java.time.DayOfWeek
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TodayRecordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TodayRecordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentTodayRecordBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var calendarAdapter: CalendarAdapter
+    private var calendarList = ArrayList<CalendarVO>()
+
+    companion object{
+        fun newInstance() = TodayRecordFragment()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today_record, container, false)
+        _binding = FragmentTodayRecordBinding.inflate(inflater, container, false)
+        val root : View = binding.root
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TodayRecordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TodayRecordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var week_day:Array<String> = resources.getStringArray(R.array.calendar_day)
+
+        calendarAdapter = CalendarAdapter(calendarList)
+
+        calendarList.apply{
+            val dateFormat = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                DateTimeFormatter.ofPattern("dd").withLocale(Locale.forLanguageTag("ko"))
+            } else {
+                TODO("VERSION.SDK_INT < O")
             }
+            val monthFormat = DateTimeFormatter.ofPattern("yyyy년 MM월").withLocale(Locale.forLanguageTag("ko"))
+
+            val localDate = LocalDateTime.now().format(monthFormat)
+            binding.todayYyMmTv.text = localDate
+
+            var preSunday: LocalDateTime = LocalDateTime.now().with(
+                TemporalAdjusters.previous(
+                DayOfWeek.SUNDAY))
+
+            for (i in 0..6) {
+                Log.d("날짜만", week_day[i])
+
+                calendarList.apply {
+                    add(CalendarVO(preSunday.plusDays(i.toLong()).format(dateFormat), week_day[i]))
+                }
+                Log.d("저번 주 일요일 기준으로 시작!", preSunday.plusDays(i.toLong()).format(dateFormat))
+            }
+            binding.todayWeekCalenderRv.adapter = calendarAdapter
+            binding.todayWeekCalenderRv.layoutManager = GridLayoutManager(context, 7)
+        }
     }
+    override fun onDestroyView(){
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
