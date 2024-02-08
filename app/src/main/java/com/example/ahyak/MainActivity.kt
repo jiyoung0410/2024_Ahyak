b9e9f5eb2d8ddc3f296c563b92c59187cc06e389
jiyoung <ghkd1310@naver.com>
2024-02-08 21:28:43 +0900

    [2월 2주차 구현] 바텀 네비게이션바 설정

--- app/src/main/java/com/example/ahyak/MainActivity.kt
+++ app/src/main/java/com/example/ahyak/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ahyak
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -17,7 +18,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+
+        fun replaceFragment(fragment: Fragment) {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.containers, fragment)
+            fragmentTransaction.commit()
+        }
+
+        replaceFragment(TodayRecordFragment())
+
+        binding.bottomNavigationview.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.navigation_today -> {
+                    replaceFragment(TodayRecordFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigation_statistics -> {
+                    replaceFragment(StatisticsFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigation_calender -> {
+                    replaceFragment(CalenderFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }else -> return@setOnNavigationItemSelectedListener false
+            }
+        }
 
     }
 }
