17d67be69bc351afad407d3acc28c24cfec719d8
jiyoung <ghkd1310@naver.com>
2024-03-17 22:29:14 +0900

    [3월 2주차 구현] 메모 수정 기능 및 정도 표현 UI 변경

--- app/src/main/java/com/example/ahyak/RecordSymptoms/DegreeSymptomsActivity.kt
+++ app/src/main/java/com/example/ahyak/RecordSymptoms/DegreeSymptomsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ahyak.RecordSymptoms
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.ahyak.R
 import com.example.ahyak.databinding.ActivityDegreeSymptomsBinding
 
 class DegreeSymptomsActivity : AppCompatActivity() {
@@ -36,23 +38,33 @@ class DegreeSymptomsActivity : AppCompatActivity() {
         //강도를 선택했을 때
         //일단은 토스트 메세지만 띄움. UI 수정 예정
         binding.degree1.setOnClickListener {
-            Toast.makeText(this,"1", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"1", Toast.LENGTH_SHORT).show()
+            binding.degreeSymptomsLl.setBackgroundResource(R.drawable.light_point_radi_15dp)
+            binding.degreeSymptomsNameTv.setTextColor(Color.BLACK)
             degreecolor = 1
         }
         binding.degree2.setOnClickListener {
-            Toast.makeText(this,"2", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"2", Toast.LENGTH_SHORT).show()
+            binding.degreeSymptomsLl.setBackgroundResource(R.drawable.point_radi_15dp)
+            binding.degreeSymptomsNameTv.setTextColor(Color.WHITE)
             degreecolor = 2
         }
         binding.degree3.setOnClickListener {
-            Toast.makeText(this,"3", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"3", Toast.LENGTH_SHORT).show()
+            binding.degreeSymptomsLl.setBackgroundResource(R.drawable.light_deep_point_radi_15dp)
+            binding.degreeSymptomsNameTv.setTextColor(Color.WHITE)
             degreecolor = 3
         }
         binding.degree4.setOnClickListener {
-            Toast.makeText(this,"4", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"4", Toast.LENGTH_SHORT).show()
+            binding.degreeSymptomsLl.setBackgroundResource(R.drawable.reguler_deep_point_radi_15dp_)
+            binding.degreeSymptomsNameTv.setTextColor(Color.WHITE)
             degreecolor = 4
         }
         binding.degree5.setOnClickListener {
-            Toast.makeText(this,"5", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"5", Toast.LENGTH_SHORT).show()
+            binding.degreeSymptomsLl.setBackgroundResource(R.drawable.deep_point_radi_15dp)
+            binding.degreeSymptomsNameTv.setTextColor(Color.WHITE)
             degreecolor = 5
         }
 
--- app/src/main/java/com/example/ahyak/RecordSymptoms/RecordSymptomsActivity.kt
+++ app/src/main/java/com/example/ahyak/RecordSymptoms/RecordSymptomsActivity.kt
@@ -1,8 +1,12 @@
 package com.example.ahyak.RecordSymptoms
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.ahyak.MainActivity
 import com.example.ahyak.databinding.ActivityRecordSymptomsBinding
@@ -25,6 +29,33 @@ class RecordSymptomsActivity : AppCompatActivity() {
         binding.recordSymptomsAddLl.setOnClickListener {
             val intent = Intent(this, SearchSymptomsActivity::class.java)
             startActivity(intent)
+        }
+
+        //더 기록하고 싶나요를 클릭하면
+        binding.recordSymptomsTv.setOnLongClickListener {
+            binding.recordSymptomsOkIc.visibility = View.VISIBLE
+            binding.recordSymptomsTv.visibility = View.INVISIBLE
+            binding.recordSymptomsEt.visibility = View.VISIBLE
+            binding.recordSymptomsEt.requestFocus() // EditText에 포커스를 설정하여 입력 가능하도록 함
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(binding.recordSymptomsEt, InputMethodManager.SHOW_IMPLICIT) // 키보드를 자동으로 표시
+            true // Long Click 이벤트를 소비하여 다른 클릭 이벤트가 발생하지 않도록 함
+        }
+
+        binding.recordSymptomsOkIc.setOnClickListener {
+            val texted = binding.recordSymptomsEt.text
+            if (texted.isEmpty()) {
+                binding.recordSymptomsTv.text = "증상에 관해 자유롭게 기록해보세요"
+            } else {
+                binding.recordSymptomsTv.text = texted
+            }
+            binding.recordSymptomsEt.clearFocus() // EditText의 포커스 제거
+            binding.recordSymptomsOkIc.visibility = View.INVISIBLE
+            binding.recordSymptomsTv.visibility = View.VISIBLE
+            binding.recordSymptomsEt.visibility = View.INVISIBLE
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.recordSymptomsEt.windowToken, 0) // 키보드 숨김
+
         }
 
         val degreeColorData = intent.getSerializableExtra("degreeColor") as? DataItemRecordSymptom
