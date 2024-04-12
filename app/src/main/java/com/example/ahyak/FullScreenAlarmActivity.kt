1b2dd30f2f0e2e1ccfd2110ad12962c46d0a4941
star8041 <kms07001@gmail.com>
2024-04-12 20:17:16 +0900

    [4월 2주차] 알람 xml 구성

---
+++ app/src/main/java/com/example/ahyak/FullScreenAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ahyak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullScreenAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_alarm)
+    }
+}
