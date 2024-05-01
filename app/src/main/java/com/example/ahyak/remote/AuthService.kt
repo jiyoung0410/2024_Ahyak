b39de7329a999f3530a241c2afc272ca2eada8fa
jiyoung <ghkd1310@naver.com>
2024-05-01 17:06:09 +0900

    [5/1]API 연결

--- app/src/main/java/com/example/ahyak/remote/AuthService.kt
+++ app/src/main/java/com/example/ahyak/remote/AuthService.kt
@@ -1,4 +1,31 @@
 package com.example.ahyak.remote
 
-class AuthService {
+import android.content.Context
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
+
+class AuthService(private val context: Context) {
+    private val authService = ApplicationClass.retrofit.create(RetroInterface::class.java)
+
+    private lateinit var drugSearchNameView: DrugSearchNameView
+
+    fun setdrugSearchNameView(drugSearchNameView: DrugSearchNameView){
+        this.drugSearchNameView = drugSearchNameView
+    }
+
+    fun drugSearchName(query:String){
+        drugSearchNameView.DrugSearchNameLoading()
+        val request = DrugSearchNameRequest(query)
+        authService.drugsearchNamePost(request).enqueue(object : Callback<BaseResponse<DrugSearchNameResponse>>{
+            override fun onResponse(
+                call : Call<BaseResponse<DrugSearchNameResponse>>,
+                response: Response<BaseResponse<DrugSearchNameResponse>>
+            ){
+                val resp = response.body()
+
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/example/ahyak/remote/RetroInterface.kt
+++ app/src/main/java/com/example/ahyak/remote/RetroInterface.kt
@@ -6,7 +6,7 @@ import retrofit2.http.POST
 
 interface RetroInterface {
     @POST("drugsearch_name")
-    fun signup(
+    fun drugsearchNamePost(
         @Body request: DrugSearchNameRequest //내부에 포함할 데이터
     ) : Call<BaseResponse<DrugSearchNameResponse>> //반환할 데이터
 }
