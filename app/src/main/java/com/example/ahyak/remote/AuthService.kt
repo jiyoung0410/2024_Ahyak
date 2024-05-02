c9498e0f84675916be530b40aad1f5ac22cedb99
star8041 <kms07001@gmail.com>
2024-05-02 18:00:13 +0900

    임시 저장

--- app/src/main/java/com/example/ahyak/remote/AuthService.kt
+++ app/src/main/java/com/example/ahyak/remote/AuthService.kt
@@ -1,9 +1,10 @@
 package com.example.ahyak.remote
 
 import android.content.Context
+import com.example.ahyak.ApplicationClass
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
+import retrofit2.Callback
 
 class AuthService(private val context: Context) {
     private val authService = ApplicationClass.retrofit.create(RetroInterface::class.java)
@@ -17,13 +18,17 @@ class AuthService(private val context: Context) {
     fun drugSearchName(query:String){
         drugSearchNameView.DrugSearchNameLoading()
         val request = DrugSearchNameRequest(query)
-        authService.drugsearchNamePost(request).enqueue(object : Callback<BaseResponse<DrugSearchNameResponse>>{
+        authService.drugsearchNamePost(request).enqueue(object : Callback<DrugSearchNameResponse> {
             override fun onResponse(
-                call : Call<BaseResponse<DrugSearchNameResponse>>,
-                response: Response<BaseResponse<DrugSearchNameResponse>>
+                call : Call<DrugSearchNameResponse>,
+                response: Response<DrugSearchNameResponse>
             ){
                 val resp = response.body()
+                // onResponse 처리
+            }
 
+            override fun onFailure(call: Call<DrugSearchNameResponse>, t: Throwable) {
+                // onFailure 처리
             }
         })
     }
--- app/src/main/java/com/example/ahyak/remote/BaseResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ahyak.remote
-
-import com.google.gson.annotations.SerializedName
-
-data class BaseResponse <T>(
-    @SerializedName("RESULT") val result : T, //자료형 명시X 아무거나 들어와도 걍 쓰겠다.
-)
